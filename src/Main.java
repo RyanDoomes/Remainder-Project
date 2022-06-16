4adf492a817c6ccf3245a3dff29a14112623bcf4
RyanDoomes <101357291+RyanDoomes@users.noreply.github.com>
2022-06-16 12:33:43 -0700

    Extra credit part
    added error message for numbers less than 1

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,31 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        boolean running = true;
 
-        System.out.println("Please enter a number greater than 1: ");
-        int n = input.nextInt();
-        int count = 1;
-        int sum = 0;
-
-        while (count <= n) {
-            if (n % count == 0) {
-                sum += count;
-
+        while (running) {
+            System.out.println("Please enter a number greater than 1: ");
+            int n = input.nextInt();
+            if (n<1){
+                System.out.println("Number has to be greater than 1, Please enter new number: ");
             }
-            count++;
+            else {
+            int count = 1;
+            int sum = 0;
+
+            while (count <= n) {
+                if (n % count == 0) {
+                    sum += count;
+
+                }
+                count++;
+
 
 
         }
         System.out.println("The sum of all factors is " + sum);
+            running=false;
+            }
+    }
     }
 }
